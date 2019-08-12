4319dda5810fec8aac70f1a1c4c7cc2e699ef455
Ricardo Luna <rluna@lilly.com>
2019-08-12 15:25:45 -0400

    working log in

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const users = require('./users');
+
+const { jwtVerify } = require('../handlers');
 
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  const jwt = req.cookies.token;
+  const isValid = jwtVerify(jwt);
+  res.render('index', { layout: 'default', role: isValid.aud });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', { layout: 'default' });
+});
+
+router.get('/logout', (req, res) => {
+  const cookie = req.cookies;
+  if (cookie.token) {
+    res.cookie('token', '', { expires: new Date(0) });
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
+module.exports = [router, users];
