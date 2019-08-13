01358f23a8d825d101489f598ca830110ef0df5c
Ricardo Luna <rluna@lilly.com>
2019-08-13 09:59:07 -0400

    send payload back on success even though redirect is in place

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,8 @@ router.post('/login', jsonParser, async (req, res) => {
   const authRequest = authUser(req.body);
   if (authRequest.success) {
     await res.cookie('token', authRequest.token, { httpOnly: true, expires: new Date(Date.now() + (60000) * cookieLifeSpan) });
-    // redirect accomplished on the front end due to jQuery making the the post request
-    res.send({}); // ensure jquery doesnt break
+    // redirect accomplished on the front end due to jQuery killing server-side redirect
+    res.send(authRequest); // ensure jquery doesnt break
     // res.redirect('/'); // see https://stackoverflow.com/questions/27202075/expressjs-res-redirect-not-working-as-expected
     return;
   }
