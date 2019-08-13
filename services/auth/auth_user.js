0b6ac7186115312487ccc9089c04ceaf06122faf
Ricardo Luna <rluna@lilly.com>
2019-08-13 09:59:46 -0400

    add users, refactor

--- services/auth/auth_user.js
+++ services/auth/auth_user.js
@@ -1,29 +1,47 @@
 const { jwtSign } = require('../../handlers');
 
 // our user "table"
-const user = {
-  useremail: 'someone@nowhere.com',
-  password: 'qwerty', // obviously this should be hashed
+const users = [
+  {
+    useremail: 'someone@nowhere.com',
+    password: 'qwerty', // obviously this should be hashed
+    role: 'admin',
+  },
+  {
+    useremail: 'phantom@blackhole.com',
+    password: 'foobar',
+    role: 'editor',
+  },
+  {
+    useremail: 'joedirt@mindspring.com',
+    password: 'mullethead',
+    role: 'reader',
+  },
+];
+
+const findUser = ({ useremail: usr, password: pwd }) => {
+  const foundUser = users.filter(user => user.useremail === usr && user.password === pwd);
+  return foundUser.length === 0 ? null : foundUser[0];
 };
 
-const authUser = ({ useremail, password }) => {
+const authUser = (payload) => {
+  const user = findUser(payload);
   const responseObj = {};
-  if (useremail !== user.useremail || password !== user.password) {
+  if (!user) {
     responseObj.success = false;
     responseObj.message = 'Login failed';
     return responseObj;
   }
-  const payload = {
-    data1: 'd1',
-    data2: 'd2',
+  const data = {
+    usr: user.useremail,
+    role: user.role,
     data3: 'd3',
     data4: 'd4',
   };
 
-  const token = jwtSign(payload);
   responseObj.success = true;
   responseObj.message = 'Login success';
-  responseObj.token = token;
+  responseObj.token = jwtSign(data);
   return responseObj;
 };
 
